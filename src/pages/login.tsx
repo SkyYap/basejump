5ea2eca8d80877b76c8987aaacbacfc0121abc42
SkyYap <32660903+SkyYap@users.noreply.github.com>
2023-09-28 01:17:57 +0800

    Change to show magic link too

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,4 +1,5 @@
 import LoginPassword from "@/components/dashboard/authentication/login-password";
+import LoginMagicLink from "@/components/dashboard/authentication/login-magic-link";
 import useTranslation from "next-translate/useTranslation";
 import Link from "next/link";
 import { useRouter } from "next/router";
@@ -14,6 +15,7 @@ const LoginPage = () => {
   return (
     <div className="max-w-md mx-4 md:mx-auto my-12 bg-base-500 grid gap-y-4">
       <LoginPassword />
+      <LoginMagicLink />
       <Link href="/signup" passHref>
         {t("shared.notYetRegistered")}
       </Link>
